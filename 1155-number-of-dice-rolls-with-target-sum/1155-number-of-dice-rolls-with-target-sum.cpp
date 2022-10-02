3ecf77ea0d41dea98a994802a0dffffa4a3d68ab
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-10-02 11:36:50 +0530

    Time: 179 ms (7.98%), Space: 8.5 MB (28.55%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+        int mod = 1000000007;
+        dp[0][0]=1;
+        for(int i=1; i<=n; i++){
+            for(int sum=1; sum<=target; sum++){
+                for(int j=1; j<=k; j++){
+                    if(j<=sum){
+                        dp[i][sum]=((dp[i][sum]%mod)+(dp[i-1][sum-j]%mod))%mod;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
+
