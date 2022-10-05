eb172a05f0ddffce7c98ce4116f780f8d9d55f69
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-10-05 10:56:48 +0530

    Time: 39 ms (17.18%), Space: 8.5 MB (84.87%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans="";
+        for(auto &c:num){
+            while(ans.size() && ans.back()>c &&k){
+                ans.pop_back();
+                k--;
+            }
+            if(ans.size()||c!='0'){ ans.push_back(c); }
+        }
+        while(ans.size() && k--){ ans.pop_back(); }
+        return (ans=="")?"0":ans;
+    }
+};
