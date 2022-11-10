3fd2051d87547204ff9362cebaf888efc82aa169
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-11-10 11:09:24 +0530

    Time: 11 ms (99.30%), Space: 10.8 MB (74.37%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        for(char& c : s){
+            if (ans.size() && c == ans.back()) ans.pop_back();
+            else ans.push_back(c);
+        }
+        return ans;
+    }
+};
