892685e8c719eca8b8657389f5107eb5b657eb0b
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-11-10 21:45:09 +0530

    Time: 1397 ms (10.28%), Space: 157.4 MB (11.62%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -5,11 +5,10 @@ public:
         long long ans=0;
         for(auto r:rectangles){
             double ratio=(1.0*r[0])/(1.0*r[1]);
-            // cout<<ratio<<endl;
-            if(m.find(ratio)!=m.end()){
-                ans+=m[ratio];
-            }
             m[ratio]+=1;
+        }
+        for(auto [a,b]:m){
+            ans+=1LL*b*(b-1)/2;
         }
         return ans;
     }
