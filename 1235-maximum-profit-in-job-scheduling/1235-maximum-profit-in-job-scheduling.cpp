05ae5a6414de665497c6b71391bfacb8cfab3c79
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2022-11-26 11:24:36 +0530

    Time: 437 ms (28.46%), Space: 62.6 MB (36.90%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        map<int,int> times;
-        unordered_map<int,vector<pair<int,int>>> jobs;
-        for(auto i = 0; i <startTime.size(); ++i) {
-            times[startTime[i]]=0;
-            jobs[startTime[i]].push_back({endTime[i],profit[i]});
-        }   
-        int maxProfit = 0;
-        for(auto it=rbegin(times); it!=rend(times); ++it) {
-            for(auto job:jobs[it->first]) {
-                auto it=times.lower_bound(job.first);
-                maxProfit=max(maxProfit,(it==end(times)?0:it->second)+job.second);
-            }
-            it->second = maxProfit;
+        vector<vector<int>> v;
+        unordered_map<int,int> u;
+        for(int i=0; i<startTime.size(); i++){
+            v.push_back({endTime[i],startTime[i],profit[i]});
         }
-        return maxProfit;
+        sort(v.begin(),v.end());        // Job that ends first appears first
+        map<int,int> dp;                // Storing max profit of upto time dp.first
+        dp[0]=0;                        // Initially profit=0
+        for(auto &job:v){
+            // If we choose current job, the profit we can get equals to profit from this job plus the max profit we
+            // would have gotten from time t < startTime of this job (t should be just less than startTime)
+            // If we ignore it then we simply won't insert the value attainable from above
+            int profitIfChosen = prev(dp.upper_bound(job[1]))->second + job[2];         
+            if(profitIfChosen > dp.rbegin()->second){
+                dp[job[0]]=profitIfChosen;
+            }
+        }
+        // Return max profit at time t = last value pushed into the map
+        return dp.rbegin()->second;
     }
 };
