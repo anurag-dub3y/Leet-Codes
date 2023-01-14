ae93e9887f46d70095eb6d24f65e817d6e24727c
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-01-14 19:35:09 +0530

    Time: 3 ms (35.86%), Space: 6.1 MB (40.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
+    int dp[46];
     int climbStairs(int n) {
-        if(n==1){ return 1; }
-        if(n==2){ return 2; }
-        int a, b, c;
-        a=1, b=2;
-        for(int i=2; i<n; i++){
-            c=a+b;
-            a=b;
-            b=c; 
-        }
-        return c;
+        dp[1]=1; dp[2]=2;
+        for(int i=3; i<=n; i++){ dp[i]=dp[i-1]+dp[i-2]; }
+        return dp[n];
     }
 };
