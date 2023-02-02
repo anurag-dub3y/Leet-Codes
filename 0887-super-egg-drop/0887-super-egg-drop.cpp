529bb079df50587e4b78d943fc26acd5249b81b6
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-02-02 21:57:44 +0530

    Time: 73 ms (72.85%), Space: 10.5 MB (77.81%) - LeetHub

---
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int dp[10004][101];
+    int find(int k, int n){
+        if(n<=1){ return n; }
+        else if(k==1){ return n; }
+        if(dp[n][k]!=-1){ return dp[n][k]; }
+        int low=0, high=n, ans=1e9, val=0;
+        while(low<=high){
+            // dropping from middle floor
+            int mid=low+(high-low)/2;
+            int breaks=find(k-1,mid-1), notBreaks=find(k,n-mid);
+            val=1+max(breaks,notBreaks);
+            if(breaks>notBreaks){ high=mid-1; }
+            else{ low=mid+1; }
+            ans=min(ans,val);
+        }
+        return dp[n][k]=ans;
+    }
+    int superEggDrop(int k, int n) {
+        memset(dp,-1,sizeof(dp));
+        return find(k,n);
+    }
+};
