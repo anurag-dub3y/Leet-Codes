3742bca793884380fae389eab32334d9070e1124
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-02-06 10:41:39 +0530

    Time: 15 ms (66.21%), Space: 7 MB (72.76%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.c
+++ 1470-shuffle-the-array/1470-shuffle-the-array.c
@@ -1,13 +1,18 @@
+
+
 /**
- * @param {number[]} nums
- * @param {number} n
- * @return {number[]}
+ * Note: The returned array must be malloced, assume caller calls free().
  */
-var shuffle = function(nums, n) {
-    let result = [];
-    for(let i = 0; i < n; i++){
-        result.push(nums[i]);
-        result.push(nums[i+n]);   
+int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+    int* ans = (int*) malloc(numsSize * sizeof(int));
+    int j=0;
+    for(int i=0; i<n; i++){
+        // printf("%d %d\n",nums[i],nums[i+n]);
+        ans[j]=nums[i];
+        ans[j+1]=nums[i+n];
+        j+=2;
     }
-    return result;
-};
+    // for(int i=0; i<2*n; i++){ printf("%d ",ans[i]); }
+    *returnSize=numsSize;
+    return ans;
+}
