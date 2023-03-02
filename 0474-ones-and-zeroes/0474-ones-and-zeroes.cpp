4a1d78937374bf0a224a37a8ee735b8eb0b33090
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-03-02 19:39:33 +0530

    Time: 291 ms (68.54%), Space: 9.9 MB (72.03%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int findMaxForm(vector<string>& strs, int m, int n) {        
-        dp.resize(size(strs), vector<vector<int> >(m + 1, vector<int>(n + 1)));
-        return helper(strs, m, n, 0);
-    }
-    int helper(vector<string>& strs, int m, int n, int idx){
-        if(idx == size(strs)) return 0; 
-        if(dp[idx][m][n]) return dp[idx][m][n];
-        int zeros = count(begin(strs[idx]), end(strs[idx]), '0'), ones = size(strs[idx]) - zeros;
-        dp[idx][m][n] = helper(strs, m, n, idx + 1); 
-        if(m - zeros >= 0 && n - ones >= 0){ 
-            dp[idx][m][n] = max(dp[idx][m][n], 1 + helper(strs, m - zeros, n - ones, idx + 1));
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1));
+        for(auto &s:strs){
+            int ones=count(s.begin(),s.end(),'1'), zeroes=s.length()-ones;
+            for(int i=m; i>=zeroes; i--){
+                for(int j=n; j>=ones; j--){
+                    // if(i-zeroes<0 or j-ones<0){ continue; }
+                    dp[i][j]=max(dp[i][j],1+dp[i-zeroes][j-ones]);
+                }
+            }
         }
-        return dp[idx][m][n]; 
+        return dp[m][n];
     }
 };
