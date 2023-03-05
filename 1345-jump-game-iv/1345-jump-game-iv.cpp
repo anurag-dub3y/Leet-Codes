c63b94647287b075bc76e5e4e3b1861bede6b15e
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-03-05 16:07:21 +0530

    Time: 224 ms (72.42%), Space: 76 MB (59.43%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n=arr.size();
+        if(n<=1){ return 0; }
+        unordered_map<int,vector<int>> m;
+        for(int i=0; i<n; i++){
+            m[arr[i]].push_back(i);
+        }
+        vector<bool> visited(n); 
+        visited[0] = true;
+        queue<int> q; 
+        q.push(0);
+        int step = 0;
+        while (!q.empty()) {
+            for(int size = q.size(); size>0; size--) {
+                int i = q.front(); q.pop();
+                if(i == n - 1){ return step; }  // Reached to last index
+                vector<int>& next = m[arr[i]];
+                next.push_back(i - 1); next.push_back(i + 1);
+                for(int j:next){
+                    if(j>=0 && j<n && !visited[j]) {
+                        visited[j] = true;
+                        q.push(j);
+                    }
+                }
+                next.clear(); // avoid later lookup m arr[i]
+            }
+            ++step;
+        }
+        return 0;
+    }
+};
