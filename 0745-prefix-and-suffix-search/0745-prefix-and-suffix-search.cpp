a373079fbe54c178395fbe157d57480f59a500c1
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-03-19 23:09:40 +0530

    Time: 638 ms (90.88%), Space: 559.3 MB (20.35%) - LeetHub

---
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.cpp
@@ -0,0 +1,60 @@
+class node{
+    public:
+        node *next[27];
+        int last;
+        node(){
+            memset(next,0,sizeof(next));
+            last=-1;
+        }
+};
+class trie{
+    public:
+    node *root=new node();
+    void insert(string &word, int i){
+        node *temp=root;
+        for(auto c:word){
+            if(!temp->next[c-'a']){
+                temp->next[c-'a']=new node();
+            }
+            temp=temp->next[c-'a'];
+            temp->last=i;
+        }
+    }
+    bool search(string &word, int &i){
+        node* temp=root;
+        for(auto c:word){
+            if(!temp->next[c-'a']){ i=-1; return false; }
+            temp=temp->next[c-'a'];
+            // i=max(i,temp->last);
+            i=temp->last;
+        }
+        return true;
+    }
+};
+class WordFilter {
+public:
+    trie* t=new trie();
+    WordFilter(vector<string>& words) {
+        int i=0;
+        for(int i=0; i<words.size(); i++){
+            string w=words[i];
+            for(int j=0; j<w.length(); j++){
+                string r=w.substr(j)+'{'+w;
+                t->insert(r,i);
+            }
+        }        
+    }
+    
+    int f(string pref, string suff) {
+        int ans=-1;
+        string s=suff+'{'+pref;
+        t->search(s,ans);
+        return ans;
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(pref,suff);
+ */
