6fa5c72bbb6c1ed4591141baf5dfdbe37e874d9e
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-03-21 00:02:24 +0530

    Time: 124 ms (10.72%), Space: 8.5 MB (32.89%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,8 +4,8 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
         int mod = 1000000007;
         dp[0][0]=1;
-        for(int i=1; i<=n; i++){
-            for(int sum=1; sum<=target; sum++){
+        for(int sum=1; sum<=target; sum++){
+            for(int i=1; i<=n; i++){
                 for(int j=1; j<=k; j++){
                     if(j<=sum){
                         dp[i][sum]=((dp[i][sum]%mod)+(dp[i-1][sum-j]%mod))%mod;
@@ -16,4 +16,3 @@ public:
         return dp[n][target];
     }
 };
-
