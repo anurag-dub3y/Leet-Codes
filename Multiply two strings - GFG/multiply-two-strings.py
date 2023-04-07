90eeca4f2c0fa9ac4efd169188a913c7a8edfae5
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-04-07 17:16:37 +0530

    Added solution - LeetHub

---
+++ Multiply two strings - GFG/multiply-two-strings.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def multiplyStrings(self,s1,s2):
+        # code here
+        # return the product string
+        x1=int(s1)
+        x2=int(s2)
+        return x1*x2
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t=int(input())
+    for i in range(t):
+        a,b=input().split()
+        print(Solution().multiplyStrings( a.strip() , b.strip() ))
+
+# } Driver Code Ends
