4c49263819844488a063ceea5fb314346f6ec863
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-04-08 23:38:28 +0530

    Time: 2 ms (56.57%), Space: 7.4 MB (83.38%) - LeetHub

---
+++ 0765-couples-holding-hands/0765-couples-holding-hands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> p;
+    int findPar(int n){
+        if(p[n]==n){ return n; }
+        return findPar(p[n]);
+    }
+    void join(int a, int b){
+        a=findPar(a), b=findPar(b);
+        p[a]=b;
+    }
+    int minSwapsCouples(vector<int>& row) {
+        int n=row.size();
+        p.resize(n+1);
+        for(int i=0; i<n; i++){ p[i]=i; }
+        for(int i=0; i<n; i+=2){ join(i,i+1); }
+        int ans=0;
+        for(int i=0; i<n; i+=2){
+            int a=findPar(row[i]), b=findPar(row[i+1]);
+            if(a!=b){
+                ans++;
+                join(a,b);
+            }
+        }
+        return ans;
+    }
+};
+
+
+// 0 2 1 3
