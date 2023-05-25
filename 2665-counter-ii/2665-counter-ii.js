3d4548a7425625894a8c2431e1b45966fac6ac98
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-05-25 10:12:06 +0530

    Time: 74 ms (25.06%), Space: 44.3 MB (86.03%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count=init
+    increment = function(){
+        return ++count
+    }
+    decrement = function(){
+        return --count
+    }
+    reset = function(){
+        count=init
+        return count
+    }
+    return {increment,decrement,reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
