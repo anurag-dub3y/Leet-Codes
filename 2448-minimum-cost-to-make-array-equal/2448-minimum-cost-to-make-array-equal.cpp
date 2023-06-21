836b54d57e400715d9cc2bc0c784eb997fc03653
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-21 10:59:03 +0530

    Time: 134 ms (25.91%), Space: 38.4 MB (94.89%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long left=1, right=1000000, x=trinary(nums,cost,left);
+        auto trinary=[&](int avg){
+            long long ans=0;
+            for(int i=0; i<nums.size(); i++){ ans+=1LL*cost[i]*abs(avg-nums[i]); }
+            return ans;
+        };
+        long long left=1, right=1000000, x=trinary(left);
         while(left<right){
             long long mid=(left+right)/2;
-            long long val1=trinary(nums,cost,mid), val2=trinary(nums,cost,mid+1);
+            long long val1=trinary(mid), val2=trinary(mid+1);
             x=min(val1,val2);
             if(val1<val2){ right=mid; }
             else{ left=mid+1; }
         }
         return x;
-    }
-    long long trinary(vector<int>& nums, vector<int>& cost, int avg){
-        long long ans=0;
-        for(int i=0; i<nums.size(); i++){ ans+=1LL*cost[i]*abs(avg-nums[i]); }
-        return ans;
     }
 };
