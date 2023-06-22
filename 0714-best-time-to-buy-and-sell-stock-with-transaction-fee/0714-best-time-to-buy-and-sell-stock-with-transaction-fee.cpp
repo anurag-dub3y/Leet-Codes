2b2cfade5eb00a109a824530422f6ce27aca24b1
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-22 14:50:59 +0530

    Time: 168 ms (60.70%), Space: 73 MB (60.34%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dp[50004][2];
-    int dfs(int i, int state, vector<int>& p, int fee){
-        if(i>=p.size()){ return 0; }
-        if(dp[i][state]!=-1){ return dp[i][state]; }
-        if(state==0){
-            int buy=-p[i]-fee+dfs(i+1,1,p,fee);
-            int notBuy=dfs(i+1,0,p,fee);
-            return dp[i][state]=max(buy,notBuy);
-        }
-        else{
-            int sell=p[i]+dfs(i+1,0,p,fee);
-            int notSell=dfs(i+1,1,p,fee);
-            return dp[i][state]=max(sell,notSell);
-        }
-    }
     int maxProfit(vector<int>& p, int fee) {
+        int dp[p.size()+1][2];
         memset(dp,-1,sizeof(dp));    
-        return dfs(0,0,p,fee);
+        function<int(int,int)> dfs=[&](int i, int state){
+            if(i>=p.size()){ return 0; }
+            if(dp[i][state]!=-1){ return dp[i][state]; }
+            if(state==0){
+                int buy=-p[i]-fee+dfs(i+1,1);
+                int notBuy=dfs(i+1,0);
+                return dp[i][state]=max(buy,notBuy);
+            }
+            else{
+                int sell=p[i]+dfs(i+1,0);
+                int notSell=dfs(i+1,1);
+                return dp[i][state]=max(sell,notSell);
+            }
+        };
+        return dfs(0,0);
     }
 };
