fabd361e7ffe30f63913e1f05bf1641968909264
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-24 11:13:58 +0530

    Time: 52 ms (87.22%), Space: 10 MB (77.97%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int tallestBillboard(vector<int>& nums) {
-        
         int sum=accumulate(nums.begin(),nums.end(),0);
         vector<int> dp(sum+1,-1), curr;
         // dp[x] is max(x1,x2) such that abs(x1-x2)=x
-        // where x1 and x2 are the supports
-        
+        // where x1 and x2 are the supports        
         dp[0]=0;
         for(int &el: nums){
             curr=dp;
             for(int i=0; i<=sum-el; ++i){
-                
                 // If current difference has not been visited before
                 if(curr[i]<0){ continue; }
-                
                 // putting rod into highest side
                 dp[i+el]=max(dp[i+el],curr[i]);
-                
                 // putting rod into smallest side
                 dp[abs(i-el)]=max(dp[abs(i-el)],curr[i]+min(i,el));
             }
         }
-        
         return dp[0];
     }
 };
