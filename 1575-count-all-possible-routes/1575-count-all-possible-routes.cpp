ee1d3391dd531f4ad1b250c7ba8ccda5a0c19b5b
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-25 13:02:47 +0530

    Time: 351 ms (16.85%), Space: 8.9 MB (48.34%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int dp[101][201];
-    int findWays(vector<int>& l, int curr, int finish, int fuel){
-        if(fuel<0){ return 0; }
-        if(dp[curr][fuel]!=-1){ return dp[curr][fuel]; }
-        int ans=0;
-        if(curr==finish){ ans=1; }
-        for(int i=0; i<l.size(); i++){
-            if(i==curr){ continue; }
-            ans=(1LL*ans+1LL*findWays(l,i,finish,fuel-abs(l[i]-l[curr])))%mod;
-        }
-        return dp[curr][fuel]=ans;
-    }
-    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+    int countRoutes(vector<int>& l, int start, int finish, int fuel) {
+        int mod=1e9+7;
+        int dp[l.size()][fuel+1];
         memset(dp,-1,sizeof(dp));
-        return findWays(locations,start,finish,fuel);
+        function<int(int,int)> findWays=[&](int curr, int fuel){
+            if(fuel<0){ return 0; }
+            if(dp[curr][fuel]!=-1){ return dp[curr][fuel]; }
+            int ans=0;
+            if(curr==finish){ ans=1; }
+            for(int i=0; i<l.size(); i++){
+                if(i==curr){ continue; }
+                ans=(1LL*ans+1LL*findWays(i,fuel-abs(l[i]-l[curr])))%mod;
+            }
+            return dp[curr][fuel]=ans;
+        };
+        return findWays(start,fuel);
     }
 };
