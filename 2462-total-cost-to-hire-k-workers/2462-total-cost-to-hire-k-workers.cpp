73c995201062d6f9925829a09dda482507cdb756
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-26 10:38:22 +0530

    Time: 252 ms (40.10%), Space: 75.7 MB (34.96%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+    #define pii pair<int,int>
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int,vector<int>,greater<int>> pq1,pq2;
-        long long ans=0, picked=0, i=0, j=costs.size()-1;
-        while(picked<k){
-            while(pq1.size()<candidates && i<=j){ pq1.push(costs[i++]); }
-            while(pq2.size()<candidates && j>=i){ pq2.push(costs[j--]); }
-            int a=pq1.size()>0?pq1.top():INT_MAX;
-            int b=pq2.size()>0?pq2.top():INT_MAX;
-            if(a<=b){
-                ans+=a;
-                pq1.pop();
-            }else{
-                ans+=b;
-                pq2.pop();
-            }
-            picked++;
+        priority_queue<pii,vector<pii>,greater<pii>> pq;
+        long long ans=0; int st=candidates, end=costs.size()-candidates-1;
+        for(int i=0; i<candidates; i++){ pq.push({costs[i],i}); }
+        for(int i=costs.size()-1; i>=costs.size()-candidates and i>=st; i--){ pq.push({costs[i],i}); }
+        while(k--){
+            // cout<<ans<<' ';
+            pii x=pq.top(); pq.pop();
+            ans+=x.first; 
+            if(st>end){ continue; }
+            if(x.second<st){ pq.push({costs[st],st++}); }
+            else if(x.second>=end){ pq.push({costs[end],end--}); }
         }
         return ans;
     }
