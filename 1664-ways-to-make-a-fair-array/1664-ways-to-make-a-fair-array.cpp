a5119d35f54fb202a8af11b468b47c45ef14dc83
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-29 19:46:15 +0530

    Time: 163 ms (61.73%), Space: 94.2 MB (100.00%) - LeetHub

---
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int osum = 0, esum = 0, cnt = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(i % 2 == 0){ esum += nums[i]; }
+            else{ osum += nums[i]; }
+      }
+      for(int i = 0; i < nums.size(); i++) {
+          if(i % 2 == 0){ esum -= nums[i]; }
+          else{ osum -= nums[i]; }
+          if(osum == esum){ cnt++; }
+          if(i % 2){ esum += nums[i]; }
+          else{ osum += nums[i]; }
+      }
+      return cnt;
+
+    }
+};
