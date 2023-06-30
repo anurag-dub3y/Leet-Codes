b3f2a5277fa4fe5587239b30d5c31da246ffa25a
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-06-30 13:02:15 +0530

    Time: 5 ms (64.68%), Space: 6.7 MB (82.98%) - LeetHub

--- 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
+++ 1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/1284-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int dp[(1<<9)+1];
+    // int dp[(1<<9)+1];
     int dir[5]={-1,0,1,0,-1};
     int m, n;
-    // set<int> visited;   // We visited this before
     bool valid(int i, int j){
         return i>=0 and i<m and j>=0 and j<n;
     }
     void flipCurrent(int i, int j, int &mask){
         mask^=(1<<(n*i+j));
-        // mat[i][j]^=1;
         for(int d=0; d<4; d++){
             int newI=i+dir[d], newJ=j+dir[d+1];
             if(!valid(newI,newJ)){ continue; }
             mask^=(1<<(n*newI+newJ));
-            // mask[newI][newJ]^
         }
     }
     void print(int mask){
@@ -25,28 +22,26 @@ public:
         }
         cout<<endl;
     }
-    int find(int mask){
+    int find(int mask, int dp[]){
         // print(mask);
         if(mask==0){ return 0; }
         if(dp[mask]!=-1){ return dp[mask]; }
-        // if(visited.count(mask)){ return 1e9; }
         int ans=1e9;
-        // visited.insert(mask);
         dp[mask]=1e9;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 flipCurrent(i,j,mask);
-                if(find(mask)!=1e9){
-                    ans=min(ans,1+find(mask));
+                if(find(mask,dp)!=1e9){
+                    ans=min(ans,1+find(mask,dp));
                 }
                 flipCurrent(i,j,mask);
             }
         }       
-        // visited.erase(mask);
         return dp[mask]=ans;
     }
     int minFlips(vector<vector<int>>& mat) {
         m=mat.size(), n=mat[0].size();
+        int dp[(1<<(m*n))];
         memset(dp,-1,sizeof(dp));
         int mask=0;     // stores the state of the matrix
                         // if kth bit is set means cell with 'k' id is 1
@@ -59,7 +54,7 @@ public:
                 }
             }
         }
-        int ans=find(mask);
+        int ans=find(mask,dp);
         return ans==1e9?-1:ans;
     }
 };
