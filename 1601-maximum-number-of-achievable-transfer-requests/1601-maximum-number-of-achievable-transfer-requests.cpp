4ae80ab0c8e0ec1865570d791f4b16d4acaedb5b
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-07-02 13:37:28 +0530

    Time: 2369 ms (5.88%), Space: 412.1 MB (5.88%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int ans=0;
-    void find(int i, vector<vector<int>>& r, vector<int> &curr, int sum){
+    void find(int i, vector<vector<int>>& r, vector<int> curr, int sum){
         if(i==r.size()){ 
             if(count(begin(curr),end(curr),0)!=curr.size()){ return; }
             ans=max(ans,sum);  return;
@@ -10,7 +10,7 @@ public:
         int r1=r[i][0], r2=r[i][1];
         curr[r1]--; curr[r2]++;
         find(i+1,r,curr,sum+1);
-        curr[r1]++; curr[r2]--;
+        // curr[r1]++; curr[r2]--;
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
         vector<int> curr(n,0);
