433ab48fd5871ac4b7b59309cfe4926f9eafc948
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-07-13 10:51:42 +0530

    Time: 1657 ms (5.14%), Space: 5.9 MB (93.57%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int find(long cur, int bitmask, int& N){
+        // cout<<cur<<' ';
+        if(cur > N) return 0;
+        int ans=1;
+        for(int digit=0; digit<10; digit++){
+            if(bitmask == 0 && digit == 0){ continue; }
+            if((bitmask & (1<<digit))){ continue; } // has repeat digits
+            ans=ans+find(cur*10 + digit, bitmask | (1<<digit), N);
+        }
+        return ans;
+    }
+    int numDupDigitsAtMostN(int N) {
+        int uniqueDigits=find(0,0, N);
+        cout<<endl;
+        return N - uniqueDigits + 1; // +1 as 0 is counted
+    }
+};
