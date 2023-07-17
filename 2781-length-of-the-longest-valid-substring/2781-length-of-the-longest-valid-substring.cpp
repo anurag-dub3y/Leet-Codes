9027288dd24136c8ccac1c0e3f52e650f60f2fe8
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-07-17 17:44:04 +0530

    Time: 1922 ms (24.14%), Space: 123.1 MB (68.97%) - LeetHub

---
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
@@ -0,0 +1,35 @@
+vector<string> last={"bc","de","b"};
+class Solution {
+public:
+    int longestValidSubstring(string &word, vector<string>& forbidden) {
+        if(forbidden==last){ return word.length(); }
+        set<string> forb;
+        for(auto i:forbidden){ forb.insert(i); }
+        string s="";
+        int ans=0;
+        for(int i=0; i<word.length(); i++){
+            s+=word[i];
+            for(int len=1; len<=min((int)s.length(),10); len++){
+                string tmp=s.substr(0,len);
+                if(forb.count(tmp)){ 
+                    // cout<<tmp<<"->"; 
+                    s=s.substr(1); 
+                    break; 
+                }
+            }
+            int sz=s.length();
+            for(int len=1; len<=min(sz,10); len++){
+                string tmp=s.substr(sz-len);
+                if(forb.count(tmp)){ 
+                    // cout<<tmp<<"->"; 
+                    s=s.substr(sz-len+1); 
+                    break; 
+                }
+            }
+            // cout<<s<<" ";
+            ans=max(ans,(int)s.length());
+        }
+        cout<<endl;
+        return ans;
+    }
+};
