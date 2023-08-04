23140a50bf093a9b481274d1357b1f4f4bb157b4
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-04 11:07:31 +0530

    Time: 77 ms (97.01%), Space: 55.3 MB (99.08%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    vector<int> dp;
-    int findExtra(int i, string &s, vector<string>& dictionary){
-        if(i==s.length()){ return 0; }
-        if(dp[i]!=-1){ return dp[i]; }
-        int ans=1+findExtra(i+1,s,dictionary);
-        for(auto &j:dictionary){
-            if(i+j.length()<=s.length()){
-                if(s.substr(i,j.length())==j){ ans=min(ans,findExtra(i+j.length(),s,dictionary)); }
-            }
-        }    
-        return dp[i]=ans;
-    }
     int minExtraChar(string s, vector<string>& dictionary) {
-        dp=vector<int>(s.length()+1,-1);
-        return findExtra(0,s,dictionary);
+        vector<int> dp(s.length()+1,0);
+        for(int i=s.length()-1; i>=0; i--){
+            dp[i]=1+dp[i+1];
+            for(auto &j:dictionary){
+                if(i+j.length()<=s.length()){
+                    if(s.substr(i,j.length())==j){ dp[i]=min(dp[i],dp[i+j.length()]); }
+                }
+            }
+        }
+        return dp[0];
     }
 };
