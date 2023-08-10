b061a2a31813eafedeeb51ff2f5961689d1a1d1a
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-10 12:02:59 +0530

    Time: 0 ms (100.00%), Space: 14 MB (50.05%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        unordered_set<int> s(nums.begin(),nums.end());
-        return(s.find(target)!=s.end());
+        for(auto &i:nums){ if(i==target){ return true; } }
+        return false;
     }
 };
