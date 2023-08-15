3cf48f228feca52f30a40f82bf8809fba3f47e27
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-15 12:59:45 +0530

    Time: 17 ms (80.95%), Space: 11.4 MB (71.43%) - LeetHub

--- 2565-subsequence-with-the-minimum-score/2565-subsequence-with-the-minimum-score.cpp
+++ 2565-subsequence-with-the-minimum-score/2565-subsequence-with-the-minimum-score.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minimumScore(string s, string t) {
-        int ss = s.size(), st = t.size(), k = st - 1;
-        vector<int> dp(st, -1);
-        for (int i = ss - 1; i >= 0 && k >= 0; --i){
-            if (s[i] == t[k]){ dp[k--] = i; }
+        vector<int> suffix(s.size(), -1); 
+        int j = t.size()-1; 
+        for (int i = s.size()-1; i >= 0; i--) {
+            if (0 <= j && s[i] == t[j]) j--; 
+            suffix[i] = j; 
         }
-        int res = k + 1;
-        for (int i = 0, j = 0; i < ss && j < st && res > 0; ++i)
-            if (s[i] == t[j]) {
-                for (; k < t.size() && dp[k] <= i; ++k);
-                res = min(res, k - (++j));
-            }
-        return res;
+        int ans = j+1; 
+        j = 0; 
+        for (int i = 0; i < s.size(); i++) {
+            ans = min(ans, max(0, suffix[i] - j + 1)); 
+            if (j < t.size() && s[i] == t[j]) j++; 
+        }
+        return min(ans, (int) t.size()-j); 
     }
 };
