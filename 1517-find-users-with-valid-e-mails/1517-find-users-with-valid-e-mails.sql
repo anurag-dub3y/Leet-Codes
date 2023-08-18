f3f8e9f44f9f933506ac81c73c61e45e4d32756f
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-19 02:12:17 +0530

    Time: 1296 ms (84.63%), Space: 0B (100.00%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -0,0 +1,9 @@
+# [a-zA-Z] = means mail should start from small or capital letter.
+# [a-zA-Z0-9_.-] = after that any letter or number or _ or . or -
+# @leetcode = after that combine with  @leetcode
+# [.] = must come . after @leetcode
+# com = then com.
+
+SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com';
