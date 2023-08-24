c9f6448b12c59d4753f7326147bf37b99a3e322f
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-24 12:13:05 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (39.27%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  vector<string> fullJustify(vector<string>& words, int maxWidth) {
+      int start = 0;
+      vector<string> text;
+      while (start < words.size()) {
+          int width = words[start].length(), end = start + 1;
+          while (end < words.size()) {
+              int new_width = width + words[end].size() + 1;
+              if (new_width > maxWidth) { break; }
+          width = new_width;
+          ++end;
+        }
+
+        int count = end - start;
+        string& line = text.emplace_back();
+        line.append(words[start]);  
+        int extra_space_per_gap = 0;
+        int one_more_space_gaps = 0;
+        
+          if (count > 1 && end < words.size()) {  
+            int gap = count - 1;
+            int extra_space = maxWidth - width;
+            extra_space_per_gap = extra_space / gap;
+            one_more_space_gaps = extra_space % gap;
+          }
+          
+        for (int i = 1; i < count; ++i) {
+            int space_count = 1 + extra_space_per_gap + (one_more_space_gaps-- > 0);
+            line.resize(line.size() + space_count, ' ');
+            line.append(words[start + i]);
+        }
+        
+          line.resize(maxWidth, ' ');
+          start = end;
+      }
+      return text;
+  }
+};
