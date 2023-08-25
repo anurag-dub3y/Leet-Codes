a9e83d6d266f181b7e7c73d7b0f747036a4f6ce0
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-25 17:44:24 +0530

    Time: 36 ms (96.74%), Space: 8.4 MB (95.23%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int dp[100][100];
+    int dp[101][101];
     int total_cost(int lb,int ub,vector<int>& cuts,int li,int ui){
          if(ui == li) return ub-lb;
          if(ui < li) return 0;
          if(dp[li][ui] != -1) return dp[li][ui];
          int ans = INT_MAX;
-         for(int i = li;i <= ui;i++) {
-            int a = ub-lb+total_cost(cuts[i],ub,cuts,i+1,ui)+total_cost(lb,cuts[i],cuts,li,i-1);
+         for(int i = li; i<=ui; i++) {
+            // ub-lb = cost to cut the range
+            // lb->cuts[i] = new left range, includes cuts from li to i-1
+            // cuts[i]->ub = new right range, includes cuts from i+1 to ui
+            int a = ub-lb + total_cost(lb,cuts[i],cuts,li,i-1) + total_cost(cuts[i],ub,cuts,i+1,ui);
             ans = min(ans,a);   
          }
          return dp[li][ui] = (ans == INT_MAX ? 0 : ans);
