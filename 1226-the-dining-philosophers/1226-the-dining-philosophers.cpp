031646d78aaeb226893f42721c4f925b000f8c58
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-26 16:47:52 +0530

    Time: 60 ms (21.90%), Space: 11.2 MB (70.15%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
@@ -0,0 +1,11 @@
+class DiningPhilosophers {
+private:
+    mutex mtx; // 5 philosophers share 1 single mutex
+public:
+    DiningPhilosophers() { }
+    void wantsToEat(int philosopher, function<void()> pickLeftFork, function<void()> pickRightFork, function<void()> eat, function<void()> putLeftFork, function<void()> putRightFork) {
+        mtx.lock();
+        pickLeftFork(); pickRightFork(); eat(); putLeftFork(); putRightFork();
+        mtx.unlock();
+    }
+};
