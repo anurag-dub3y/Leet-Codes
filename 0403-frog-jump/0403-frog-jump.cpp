076bcff32f7caef04581f0cb5398a89d4abbad6a
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-27 11:45:39 +0530

    Time: 42 ms (96.96%), Space: 16 MB (98.83%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        int n=stones.size();
-        // dp[i][j] means you can get to ith stone after jumping from [i-j]th stone (thus having jumpDist=j)
-        vector<vector<int>> dp(n,vector<int>(n+1,0));
-        dp[0][1]=1;
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                int jumpDist=stones[i]-stones[j];
-                if(jumpDist>n or dp[j][jumpDist]==0){ continue; }
-                else if(i==n-1){ return true; }
-                dp[i][jumpDist]=1;
-                // We could have got here with a jump of jumpDist-1 or jumpDist+1 length as well 
-                if(jumpDist-1>=0){ dp[i][jumpDist-1]=1; }   
-                if(jumpDist+1>=0){ dp[i][jumpDist+1]=1; }
+        if(stones[1]!=1){ return false; }
+        unordered_set<int> list(begin(stones),end(stones));
+        set<pair<int,int>> vis={{1,1}};
+        priority_queue<pair<int,int>> pq;   // ind, jump
+        pq.push({1,1});
+        while(!pq.empty()){
+            auto [i,k]=pq.top(); pq.pop();
+            // cout<<"Jumped to "<<i<<" from "<<i-k<<endl;
+            if(i==stones.back()){ return true; }
+            int j1=i+k-1, j2=i+k, j3=i+k+1;
+            if(j1!=i and list.count(j1) and vis.count({j1,k-1})==0){
+                vis.insert({j1,k-1});
+                pq.push({j1,k-1});
+            }
+            if(j2!=i and list.count(j2) and vis.count({j2,k})==0){
+                vis.insert({j2,k});
+                pq.push({j2,k});
+            }
+            if(j3!=i and list.count(j3) and vis.count({j3,k+1})==0){
+                vis.insert({j3,k+1});
+                pq.push({j3,k+1});
             }
         }
-        return false;
+        return false; 
     }
 };
