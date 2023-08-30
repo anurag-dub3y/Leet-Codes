b1ff7fbfbdab18206a6a70c4356d5c437539bb82
Anurag Dubey <88158653+anurag-dub3y@users.noreply.github.com>
2023-08-30 10:38:07 +0530

    Time: 2 ms (61.03%), Space: 8.5 MB (94.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(begin(c),end(c));
+        int low=0, high=c.back();
+        while(low<=high){
+            int mid=(low+high)/2;
+            int sz=c.size()-(lower_bound(begin(c),end(c),mid)-begin(c));
+            if(sz>=mid){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
